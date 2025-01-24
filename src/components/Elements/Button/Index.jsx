2b606b86f47eb7b22b6614379b8ae82b15986119
ilham <ilhamnopi@icloud.com>
2025-01-24 18:38:35 +0700

    refactor(Button): refactor button props change varian as className

--- src/components/Elements/Button/Index.jsx
+++ src/components/Elements/Button/Index.jsx
@@ -1,6 +1,6 @@
-const Button = ({ children, type, variant = "bg-slate-900" }) => {
+const Button = ({ children, type, className = "bg-slate-900" }) => {
     return <>
-        < button type={type} className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}> {children}</button>
+        < button type={type} className={`h-10 px-6 font-semibold rounded-md ${className} text-white`}> {children}</button>
     </>
 }
 
