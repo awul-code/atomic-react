c9b7a979548e12a3cfcad33f4c54947269b7eb4d
ilham <ilhamnopi@icloud.com>
2025-01-24 18:41:42 +0700

    feat(Login Form): create complete form page in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,27 @@ function App() {
 
   return (
     <>
-      <div className='bg-slate-800 flex gap-5 justify-center h-screen items-center flex-wrap flex-col' >
-        <h1 className="text-5xl text-white">Hello world this has using tailwind CSS 👍</h1>
-        <Button type="submit">Submit</Button>
-        <Button type="submit" variant='bg-blue-500'>Submit</Button>
-        <Button type="submit" variant='bg-pink-500'>Submit</Button>
+      <div className='bg-slate-200 h-screen flex justify-center items-center'>
+        <div className="w-full max-w-xs bg-slate-100 p-7 rounded">
+          <h1 className="text-3xl font-bold mb-8">Login</h1>
+          <p className="font-medium text-slate-500 mb-8">
+            Welcome, please enter your details
+          </p>
+          {/* FORM */}
+          <form action="">
+            <div className="mb-6">
+              <label htmlFor="email" className="block text-slate-700 mb-2" >Email</label>
+              <input type="text" className="text-sm border rouded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder="example@mail.com" />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="password" className="block text-slate-700 mb-2" >Password</label>
+              <input type="password" className="text-sm border rouded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder="********" />
+            </div>
+
+            <Button type="submit" className='w-full bg-blue-700'>SUBMIT</Button>
+          </form>
+
+        </div>
       </div>
     </>
   )
