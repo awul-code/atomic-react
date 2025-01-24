18ae024852c7ada998336da386ce1645381c410e
ilham <ilhamnopi@icloud.com>
2025-01-24 19:29:30 +0700

    refactor(Button):refactor button component

--- src/components/Elements/Button/Index.jsx
+++ src/components/Elements/Button/Index.jsx
@@ -1,6 +1,10 @@
 const Button = ({ children, type, className = "bg-slate-900" }) => {
     return <>
-        < button type={type} className={`h-10 px-6 font-semibold rounded-md ${className} text-white`}> {children}</button>
+        < button
+            type={type}
+            className={`h-10 px-6 font-semibold rounded-md ${className} text-white`}>
+            {children}
+        </button>
     </>
 }
 
