7f94ab5180167d4ca1c6abd99497e93165b952e5
ilham <ilhamnopi@icloud.com>
2025-01-25 20:47:48 +0700

    update(main): Update main.jsx to including routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginPage from './Pages/login.jsx'
+import RegisterPage from './Pages/register.jsx'
+import ErrorPage from './Pages/404.jsx'
 import './index.css'
-import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <div>Hello world</div>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/login", element: <LoginPage />
+  },
+  {
+    path: "/register", element: <RegisterPage />
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
