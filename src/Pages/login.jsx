2cbf7329f28c65e17b0a49f61b1a7e6264ef932c
ilham <ilhamnopi@icloud.com>
2025-01-25 20:52:29 +0700

    update(login register): set attribute Link to redirect pages login and register

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,11 +1,16 @@
 
 import AuthLayout from '../components/Layouts/AuthLayouts';
 import FormLogin from '../components/Fragments/FormLogin/index';
+import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
     return (<>
         <AuthLayout title="Login">
             <FormLogin />
+            <p className='mt-5 text-slate-400 text-[0.9rem]'>
+                Dont have account ?
+                <Link to="/register" className='font-bold text-blue-500 ms-1'>Register</Link>
+            </p>
         </AuthLayout>
     </>)
 }
--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -1,11 +1,16 @@
 
 import AuthLayout from '../components/Layouts/AuthLayouts';
 import FormRegister from '../components/Fragments/FormRegister';
+import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
     return (<>
         <AuthLayout title="Register">
             <FormRegister />
+            <p className='mt-5 text-slate-400 text-[0.9rem]'>
+                Dont have account ?
+                <Link to="/login" className='font-bold text-blue-500 ms-1'>Login</Link>
+            </p>
         </AuthLayout>
     </>)
 }
