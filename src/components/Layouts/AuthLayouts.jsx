cf597b1a358b257d497c4048bf380526cac15bac
ilham <ilhamnopi@icloud.com>
2025-01-25 20:50:25 +0700

    update(AuthLayout): set wrapper to  auth layout.

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,12 +1,14 @@
 
 const AuthLayout = ({ children, title }) => {
     return (<>
-        <div className="w-full max-w-xs bg-slate-100 p-7 rounded">
-            <h1 className="text-3xl text-blue-600 font-bold mb-8">{title}</h1>
-            <p className="font-medium text-slate-500 mb-8">
-                Welcome, please enter your details
-            </p>
-            {children}
+        <div className='bg-slate-200 h-screen flex justify-center items-center'>
+            <div className="w-full max-w-xs bg-slate-100 p-7 rounded">
+                <h1 className="text-3xl text-blue-600 font-bold mb-8">{title}</h1>
+                <p className="font-medium text-slate-500 mb-8">
+                    Welcome, please enter your details
+                </p>
+                {children}
+            </div>
         </div>
     </>)
 }
