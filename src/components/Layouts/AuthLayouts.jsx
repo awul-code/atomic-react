6757393e6f84f8d9d6723bd9e6f65b7f3b3153c1
ilham <ilhamnopi@icloud.com>
2025-01-25 21:58:12 +0700

    feat: Add Navigation component to AuthLayout
    - Added `type` prop to `AuthLayout` component.
    - Created `Navigation` component to conditionally render different links based on the `type` prop.
    - Updated AuthLayout to include `Navigation` component.

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,5 +1,5 @@
-
-const AuthLayout = ({ children, title }) => {
+import { Link } from 'react-router-dom';
+const AuthLayout = ({ children, title, type }) => {
     return (<>
         <div className='bg-slate-200 h-screen flex justify-center items-center'>
             <div className="w-full max-w-xs bg-slate-100 p-7 rounded">
@@ -8,8 +8,22 @@ const AuthLayout = ({ children, title }) => {
                     Welcome, please enter your details
                 </p>
                 {children}
+                <Navigation type={type} />
             </div>
         </div>
     </>)
 }
+
+const Navigation = ({ type }) => {
+    if (type === "login") {
+        return <p className='mt-5 text-slate-400 text-[0.9rem]'>
+            Dont have account ? <Link to="/register" className='font-bold text-blue-500 ms-1'>register</Link>
+        </p>
+    } else {
+        return <p className='mt-5 text-slate-400 text-[0.9rem]'>
+            Already have account ? <Link to="/login" className='font-bold text-blue-500 ms-1'>login</Link>
+        </p>
+    }
+}
+
 export default AuthLayout
