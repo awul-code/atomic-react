c8d3ae8d12566529d14a59784c7218eb3dd59280
awul <ilhamnopi@icloud.com>
2025-02-03 02:34:00 +0700

    feat: Add onClick prop to Button component
    - Added onClick prop to Button component to handle click events.
    - Updated className and onClick attributes in button element.

--- src/components/Elements/Button/Index.jsx
+++ src/components/Elements/Button/Index.jsx
@@ -1,8 +1,9 @@
-const Button = ({ children, type, className = "bg-slate-900" }) => {
+const Button = ({ children, type, className = "bg-slate-900", onClick }) => {
     return <>
         < button
             type={type}
-            className={`h-10 px-6 font-semibold rounded-md ${className} text-white`}>
+            className={`h-10 px-6 font-semibold rounded-md ${className} text-white`}
+            onClick={onClick}>
             {children}
         </button>
     </>
