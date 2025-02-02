ebed2d934b804f676a4785ba7f5adb1dd19bc70d
awul <ilhamnopi@icloud.com>
2025-02-03 02:33:09 +0700

    feat: Add handleLogin function to FormLogin component
    - Added handleLogin function to handle form submission
    - Prevents default form submission behavior
    - Retrieves email and password values from input fields
    - Stores email and password in local storage
    - Redirects to "/products" page upon successful login

--- src/components/Fragments/FormLogin/index.jsx
+++ src/components/Fragments/FormLogin/index.jsx
@@ -2,9 +2,19 @@ import InputForm from '../../Elements/Input/Index';
 import Button from '../../Elements/Button/Index';
 
 const FormLogin = () => {
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        localStorage.setItem("email", email)
+        localStorage.setItem("password", password)
+        window.location.href = "/products";
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={handleLogin}>
                 <InputForm
                     label={"Email"}
                     name={"email"}
