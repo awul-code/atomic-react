a6b0cd126a559235e2661c294d92ce92414409ee
awul <ilhamnopi@icloud.com>
2025-02-03 23:59:55 +0700

    refactor(register.jsx): remove unnecessary Link import
    Removed the unnecessary `import { Link } from 'react-router-dom';` statement in the `register.jsx` file to clean up the code and eliminate potential errors.

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -1,7 +1,6 @@
 
 import AuthLayout from '../components/Layouts/AuthLayouts';
 import FormRegister from '../components/Fragments/FormRegister';
-import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
     return (<>
