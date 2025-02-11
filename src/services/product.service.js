1eb4f8f3f55441aa40176e12c774d49539c7971f
awul <ilhamnopi@icloud.com>
2025-02-11 22:33:59 +0700

    fix: Tambahkan validasi input dan ubah searchProducts menjadi fungsi async
    - Menambahkan pengecekan untuk memastikan parameter 'query' adalah string yang tidak kosong. Jika validasi gagal, fungsi akan mengembalikan error 'Please enter a valid query'.
    - Mengubah 'searchProducts' menjadi fungsi async untuk penanganan operasi asinkron yang lebih efektif.
    
    Perbaikan ini meningkatkan keandalan fungsi pencarian dengan mencegah input tidak valid dan memastikan penanganan permintaan HTTP yang lebih efisien.

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -17,13 +17,19 @@ export const getDetailProducts = (id, callback) => {
 }
 
 
-export const searchProducts = (query, callback) => {
-    axios.get(`https://fakestoreapi.com/products?search=${query}`).then((res) => {
-        const filteredProducts = res.data.filter((product) =>
+export const searchProducts = async (query, callback) => {
+
+    if (typeof query !== 'string' || query.trim() === "") {
+        callback([], new Error("Please enter a valid query"))
+        return
+    }
+    try {
+        const response = await axios.get(`https://fakestoreapi.com/products?search=${query}`)
+        const searchResuts = response.data.filter((product) =>
             product.title.toLowerCase().includes(query.toLowerCase())
-        );
-        callback(filteredProducts);
-    }).catch((err) => {
-        console.log(err)
-    })
+        )
+        callback(searchResuts, null)
+    } catch (error) {
+        callback([], error)
+    }
 }
