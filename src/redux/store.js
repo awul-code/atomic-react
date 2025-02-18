7d9db609c2d0a81e625704bf69308a04b72ed463
awul <ilhamnopi@icloud.com>
2025-02-18 14:52:00 +0700

    feat(cart): add cartSlice to manage Redux state for cart
    - Created cartSlice to handle cart state management using Redux
    - Added actions and reducers to manage cart items and quantities
    - Ensured integration with the existing Redux store setup

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./slices/cartSlice";
+
+const store = configureStore({
+    reducer: {
+        cart: cartSlice
+    }
+})
+
+console.log("on create store:", store.getState());
+
+store.subscribe(() => {
+    console.log("STORE CHANGE:", store.getState());
+});
+
+export default store
