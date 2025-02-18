5c8ba9267385dc804101e962c690fed05ce89fdf
awul <ilhamnopi@icloud.com>
2025-02-19 00:21:44 +0700

    feat(profile): add Breadcrumb component to profile
    - Added Breadcrumb component for improved navigation in profile
    - Implemented basic structure and styling for Breadcrumb
    - Ensured seamless integration with existing profile components

--- src/Pages/profile.jsx
+++ src/Pages/profile.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useLogin } from '../Hooks/useLogin'
 import { Link } from 'react-router-dom'
+import { setBreadcrumb } from '../redux/slices/breadcrumbSlice'
+import { useDispatch } from 'react-redux'
+import Breadcrumb from '../components/Fragments/Breadcrumb'
 
 const ProfilePage = () => {
     const username = useLogin()
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setBreadcrumb({ profile: 'Profile' }));
+    }, [dispatch]);
+
     return (
         <div>
+            <Breadcrumb />
             <h1 className='text-3xl font-bold'>Profile</h1>
             <p>{username}</p>
             <Link to={"/products"}>
