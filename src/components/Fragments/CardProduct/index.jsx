80d4ab95813833507ff46c035652836835b6c5e7
awul <ilhamnopi@icloud.com>
2025-03-01 18:11:41 +0700

    feat: add handleAddToCart function to Footer component
    - Added handleAddToCart function to dispatch addToCart action with quantity 1.
    - Added alert message "Berhasil menambahkan barang ke keranjang" to notify user of successful addition to cart.
    - Updated onClick event handler to use handleAddToCart function.

--- src/components/Fragments/CardProduct/index.jsx
+++ src/components/Fragments/CardProduct/index.jsx
@@ -43,12 +43,16 @@ const Body = ({ name, children }) => {
 
 const Footer = ({ price, id }) => {
     const dispatch = useDispatch();
+    const handleAddToCart = () => {
+        dispatch(addToCart({ id, qty: 1 }))
+        alert("Berhasil manambahkan barang ke keranjang")
+    }
     return (
         <div className="flex items-center justify-between px-5 pb-5 ">
             <span className="text-xl font-bold text-white">{price}</span>
             <Button
                 className="bg-blue-600"
-                onClick={() => dispatch(addToCart({ id, qty: 1 }))}>
+                onClick={handleAddToCart}>
                 Add to chart
             </Button>
         </div>
